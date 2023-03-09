0cbf136ab0346aa0118f604288ff698aa46efcd5
ykkwn <monmon_ranma@yahoo.co.jp>
2023-03-09 22:27:21 +0900

    Lesson17: User登録機能・変更機能の追加

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,7 +1,11 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
 
@@ -17,5 +21,24 @@ public class UserService {
     public List<User> getUserList() {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
+    }
+
+    /** Userを1件検索して返す*/
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+    /** Userの登録を行う*/
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    /** Userの削除を行う*/
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
     }
 }
